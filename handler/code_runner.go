badfc20eec730ca36e4592f84c8de764ac474d16
Apale7 <1092377056@qq.com>
2021-02-07 23:16:23 +0800

    与前端联调完成

--- handler/code_runner.go
+++ handler/code_runner.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"context"
+	"fmt"
+	"strconv"
 	"web-IDE-back-end/dal/rpc"
 	"web-IDE-back-end/model"
 	"web-IDE-back-end/proto/CodeRunner"
@@ -13,17 +15,18 @@ import (
 func Judge(c *gin.Context) {
 	ctx := context.Background()
 	var req model.JudgeReq
-	if err := c.Bind(&req); err != nil {
+	if err := c.ShouldBind(&req); err != nil {
 		logrus.Warnf("Invalid params: %v", err)
 		c.String(200, "Invalid params")
 		return
 	}
 	logrus.Infof("Get JudgeReq: %+v", req)
-	resp, _ := rpc.Judge(ctx, CodeRunner.CodeLanguage(req.Language), req.Input, req.Code)
-	// if err != nil {
-	// 	logrus.Warnf("server error: %v", err)
-	// 	c.String(200, "服务器错误")
-	// 	return
-	// }
+	language, _ := strconv.Atoi(req.Language)
+	resp, err := rpc.Judge(ctx, CodeRunner.CodeLanguage(language), req.Input, req.Code)
+	if err != nil {
+		logrus.Warnf("server error: %v", err)
+		c.String(200, "服务器错误")
+		return
+	}
 	c.JSON(200, gin.H{"code": "0", "resp": resp})
 }
--- model/req.go
+++ model/req.go
@@ -1,10 +1,8 @@
 package model
 
-type CodeLanguage int32
-
 // JudgeReq 用户提交代码时的请求体
 type JudgeReq struct {
-	Language CodeLanguage `json:"language"`
-	Input    string       `json:"input"`
-	Code     string       `json:"code"`
+	Language string `json:"language"`
+	Input    string `json:"input"`
+	Code     string `json:"code"`
 }
