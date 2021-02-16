38bb8c99228ed08ec88effeba1c1d0c72a22b447
Apale7 <1092377056@qq.com>
2021-02-16 22:48:37 +0800

    readme

--- build.sh
+++ build.sh
@@ -7,7 +7,15 @@ find ./ -name "*.go" | xargs gofmt -w -s -l
 mkdir -p output/bin output/config
 cp -r config/ output/config/
 
-GOOS=windows GOARCH=amd64 go build -o output/bin/${NAME}.exe
-GOOS=darwin GOARCH=amd64 go build -o output/bin/${NAME}.out
-chmod +x output/bin/${NAME}.out
+
+name=`uname -o`
+if [[ $name =~ "Darwin" ]];then
+    GOOS=darwin GOARCH=amd64 go build -o output/bin/${NAME}.out
+    chmod +x output/bin/${NAME}.out
+elif [[ $name =~ "GNU/Linux" ]];then
+    GOOS=linux GOARCH=amd64 go build -o output/bin/${NAME}.out
+    chmod +x output/bin/${NAME}.out
+else
+    GOOS=windows GOARCH=amd64 go build -o output/bin/${NAME}.exe
+fi
 chmod +x output/start.sh
